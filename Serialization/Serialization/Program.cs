e1107d6efb801bdbb932d2567d9df5894cabb1e9
Subhamkrojha <subhamkumar51@gmail.com>
2021-09-23 02:04:02 +0530

    serilize and deserilize

---
+++ Serialization/Serialization/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Serialization
+{
+    class Program
+    {
+        public static void serilization()
+        {
+            person p1 = new person("subham", 22, "Bangalore");
+            FileStream fs = new FileStream(@"C:\Users\Subham Kumar Ojha\Desktop\serial\Serialization\serial.txt", FileMode.OpenOrCreate);
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(fs, p1);
+            fs.Close();
+            Console.WriteLine("File is created:");
+            Console.ReadLine();
+        
+    }
+        public static void deserialization()
+        {
+            person per;
+            FileStream fs = new FileStream(@"C:\Users\Subham Kumar Ojha\Desktop\serial\Serialization\serial.txt", FileMode.Open);
+            BinaryFormatter formatter = new BinaryFormatter();
+            per = (person)formatter.Deserialize(fs);
+            string name = per.name;
+            int age = per.age;
+            string city = per.city;
+            fs.Close();
+            Console.WriteLine("the name of the person is :");
+            Console.WriteLine("age is:");
+            Console.WriteLine("City is:");
+
+        }
+        static void Main(string[] args)
+        {
+            serilization();
+            deserialization();
+            Console.ReadLine();
+        } 
+    }
+}
---
+++ Serialization/Serialization/person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization
+{
+    [Serializable]
+    class person
+    {
+        public string name;
+        public int age;
+        public string city;
+    public person(string name,int age,string city)
+        {
+            this.name = name;
+            this.age = age;
+            this.city = city;
+        }
+    }
+}
